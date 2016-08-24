d64b5ef8e5130b58eacbec681594e4453786921f
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2016-08-24 16:26:50 -0700

    Fixing issues with database update scripts

--- Database/Upgrade/DDL/4.1.0f_templateOrganization.sql
+++ Database/Upgrade/DDL/4.1.0f_templateOrganization.sql
@@ -1,6 +1,14 @@
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'template' AND COLUMN_NAME = 'organizationId')
+IF EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID('template') AND name = 'IDX_template_organizationId')
 BEGIN
 	DROP INDEX IDX_template_organizationId ON dbo.template
+END
+
+IF EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID('template') AND name = 'FK_template_organization')
+BEGIN
 	ALTER TABLE dbo.template DROP CONSTRAINT FK_template_organization
+END
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'template' AND COLUMN_NAME = 'organizationId')
+BEGIN
 	ALTER TABLE dbo.template DROP COLUMN organizationId
 END
