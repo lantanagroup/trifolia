858a1646ad7cc0952f9d31204d70b82537208f5e
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2016-08-25 08:28:40 -0700

    Revert "Issues with allowing lower-case urls in dev environments. Disabling for now."
    This reverts commit 0c5b457a494fd97f2fa1f66b011d2c441b31bc98.

--- Trifolia.Web/Global.asax.cs
+++ Trifolia.Web/Global.asax.cs
@@ -20,6 +20,8 @@ namespace Trifolia.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.LowercaseUrls = true;
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.IgnoreRoute("{resource}.ashx/{*pathInfo}");
             routes.IgnoreRoute("{*botdetect}", new { botdetect = @"(.*)BotDetectCaptcha\.ashx" });
