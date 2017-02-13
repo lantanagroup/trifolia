7e970766202d248ef2589b9dee42cb5108ad0d9b
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2017-02-13 10:56:02 -0800

    Adding seed data to migratoin

--- Trifolia.DB/Migrations/Configuration.cs
+++ Trifolia.DB/Migrations/Configuration.cs
@@ -14,6 +14,56 @@ namespace Trifolia.DB.Migrations
 
         protected override void Seed(Trifolia.DB.TrifoliaDatabase context)
         {
+            context.ImplementationGuideTypes.AddOrUpdate(igt => igt.Id,
+                new ImplementationGuideType()
+                {
+                    Id = 1,
+                    Name = "CDA",
+                    SchemaLocation = "cda.xsd",
+                    SchemaPrefix = "cda",
+                    SchemaURI = "urn:hl7-org:v3"
+                },
+                new ImplementationGuideType()
+                {
+                    Id = 2,
+                    Name = "eMeasure",
+                    SchemaLocation = "schemas\\EMeasure.xsd",
+                    SchemaPrefix = "ems",
+                    SchemaURI = "urn:hl7-org:v3"
+                },
+                new ImplementationGuideType()
+                {
+                    Id = 3,
+                    Name = "HQMF R2",
+                    SchemaLocation = "schemas\\EMeasure.xsd",
+                    SchemaPrefix = "hqmf",
+                    SchemaURI = "urn:hl7-org:v3"
+                },
+                new ImplementationGuideType()
+                {
+                    Id = 4,
+                    Name = "FHIR DSTU1",
+                    SchemaLocation = "fhir-all.xsd",
+                    SchemaPrefix = "fhir",
+                    SchemaURI = "http://hl7.org/fhir"
+                },
+                new ImplementationGuideType()
+                {
+                    Id = 5,
+                    Name = "FHIR DSTU2",
+                    SchemaLocation = "fhir-all.xsd",
+                    SchemaPrefix = "fhir",
+                    SchemaURI = "http://hl7.org/fhir"
+                },
+                new ImplementationGuideType()
+                {
+                    Id = 6,
+                    Name = "FHIR DSTU3",
+                    SchemaLocation = "fhir-all.xsd",
+                    SchemaPrefix = "fhir",
+                    SchemaURI = "http://hl7.org/fhir"
+                }
+            );
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
