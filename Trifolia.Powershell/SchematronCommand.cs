b4e195b424a1e207d6dc83bc205ea218297425a5
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2017-02-23 12:56:54 -0800

    Adding powershell command to export schematron

---
+++ Trifolia.Powershell/SchematronCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Trifolia.DB;
+using Trifolia.Shared.Plugins;
+using Trifolia.Shared;
+using Trifolia.Export.Schematron;
+
+namespace Trifolia.Powershell
+{
+    [Cmdlet(VerbsCommon.Get, "TrifoliaSchematron")]
+    public class SchematronCommand : BaseCommand
+    {
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "The file name/location to output the export to. If not specified, will be output to console"
+        )]
+        public string OutputFileName { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "The id of the implementation guide to export"
+        )]
+        public int ImplementationGuideId { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "The name of the file that should be used to reference vocabulary in the schematron (ex: voc.xml)"
+        )]
+        public string VocabFileName { get; set; }
+
+        public SchematronCommand()
+        {
+            this.VocabFileName = "voc.xml";
+        }
+
+        protected override void ProcessRecord()
+        {
+            ImplementationGuide ig = this.tdb.ImplementationGuides.Single(y => y.Id == this.ImplementationGuideId);
+            SimpleSchema schema = ig.ImplementationGuideType.GetSimpleSchema();
+            IGSettingsManager igSettings = new IGSettingsManager(this.tdb, ig.Id);
+            var templates = ig.GetRecursiveTemplates(this.tdb);
+
+            string schematron = SchematronGenerator.Generate(this.tdb, ig, true, VocabularyOutputType.Default, this.VocabFileName, templates);
+
+            if (!string.IsNullOrEmpty(this.OutputFileName))
+            {
+                File.WriteAllText(this.OutputFileName, schematron);
+            }
+            else
+            {
+                this.WriteObject(schematron);
+            }
+        }
+    }
+}
