f16915369f72251332acb1b310a02ed72c03a330
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2017-03-01 07:49:54 -0800

    Creating application files manifest in Trifolia.Web during packaging

--- prepare_package.bat
+++ prepare_package.bat
@@ -7,3 +7,14 @@ xcopy /Y /E ".\Trifolia.Web\obj\%BuildConfig%\Package\PackageTmp" ".\Dist\Trifol
 xcopy /Y ".\Trifolia.Web\obj\%BuildConfig%\TransformWebConfig\transformed\Web.config" ".\Dist\Trifolia.Web\Web.config"
 xcopy ".\packages\EntityFramework.6.1.3\tools\migrate.exe" ".\Dist\Trifolia.Web\bin\"
 xcopy ".\*.ps1" ".\Dist\"
+
+@echo off
+copy /y nul Dist\manifest.txt
+setlocal disableDelayedExpansion
+for /f "delims=" %%A in ('forfiles /p .\Dist\Trifolia.Web /s /c "cmd /c echo @relpath"') do (
+  set "file=%%~A"
+  setlocal enableDelayedExpansion
+  echo !file:~2! >> Dist\manifest.txt
+  endlocal
+)
+move Dist\manifest.txt Dist\Trifolia.Web\
