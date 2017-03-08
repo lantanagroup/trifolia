f33ad01e1afc84d8ccc506f8b5731940731fac88
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2017-03-08 08:08:22 -0800

    Updated help documentation to include updated "What's new" informatio for the next release. Fixed home-page to display what's new information correctly.

--- Trifolia.Web/Scripts/Home/LoggedIn.js
+++ Trifolia.Web/Scripts/Home/LoggedIn.js
@@ -7,8 +7,7 @@
 
     $.get(url, function (data) {
         var bodyContent = data.replace(/^[\S\s]*<body[^>]*?>/i, "").replace(/<\/body[\S\s]*$/i, "");
-        var body = $(bodyContent);
-        body.find('#topic_header_nav').remove();
+        var body = $(bodyContent).find('.main-content');
         body.find('img[src]').each(function () {
             var img = this;
             var src = $(img).attr('src');
