b6269a4663f66e09423527b6256f3d432c40d3f3
michael-misiaszek <michael.misiaszek@lantanagroup.com>
2017-10-02 10:31:58 -0700

    Update Backup.ps1

--- Backup.ps1
+++ Backup.ps1
@@ -1,13 +1,11 @@
-﻿Param(
+Param(
     [Parameter(Mandatory=$True, Position=1)]
     [string] $sqlServerName,
     [Parameter(Mandatory=$True, Position=2)]
     [string] $databaseName = 'templatedb',
     [Parameter(Mandatory=$True, Position=3)]
     [string] $installDir,
-    [Parameter(Mandatory=$True, Position=4)]
-    [string] $programDataDir = 'C:\ProgramData\Trifolia',
-    [Parameter(Mandatory=$True, Position=5)]
+    [Parameter(Position=5)]
     [string] $outDir = '.\'
 )
 
@@ -18,11 +16,9 @@ Add-Type -AssemblyName System.IO.Compression
 # $sqlServerName = "PRODUCTION3"
 # $databaseName = "templatedb"
 # $installDir = "E:\Websites\TdbManagement"
-# $programDataDir = "C:\ProgramData\Trifolia"
 # $outDir = "c:\users\sean.mcilvenna"
 
 $absOutDir = (Resolve-Path $outDir -ErrorAction Stop).Path
-$absProgramDataDir = (Resolve-Path $programDataDir -ErrorAction Stop).Path
 $absInstallDir = (Resolve-Path $installDir -ErrorAction Stop).Path
 $dt = get-date -format yyyyMMddHHmmss
 $sqlBackupFileName = $databaseName + "-" + $dt + ".bak"
@@ -64,14 +60,6 @@ foreach ($file in $coreFiles) {
     $relativeFileName = $file.FullName.Replace($absInstallDir, 'Website')
 	Write-Host "Saving " $file.FullName " to ZIP at $relativeFileName"
     [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $file.FullName, $relativeFileName)
-}
-
-## Add the $programDataDir directory to the ZIP
-$programDataFiles = Get-ChildItem $absProgramDataDir -Recurse -File
-
-foreach ($file in $programDataFiles) {
-    $relativeFileName = $file.FullName.Replace($absProgramDataDir + '\', '')
-    [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $file.FullName, 'ProgramData\' + $relativeFileName)
 }
 
 ## Add the $networkBackupFilePath to the ZIP
