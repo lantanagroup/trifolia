8e5ee497388a3c0f2def0a24d82d869d57a2a6d5
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2018-03-13 10:03:41 -0700

    TRIF-1304: Add filtering and additional "Type" column to implementation guide list

--- Trifolia.Web/Scripts/IGManagement/List.js
+++ Trifolia.Web/Scripts/IGManagement/List.js
@@ -10,8 +10,8 @@
 
         switch (self.SortField()) {
             case 'Name':
-                leftVal = left.Title();
-                rightVal = right.Title();
+                leftVal = left.Title().trim();
+                rightVal = right.Title().trim();
                 break;
             case 'Status':
                 leftVal = left.Status();
@@ -35,35 +35,59 @@
                 leftVal = left.Organization();
                 rightVal = right.Organization();
                 break;
+            case 'Type':
+                leftVal = left.Type();
+                rightVal = right.Type();
+                break;
         }
 
-        if (leftVal == null && rightVal != null)
+        if (leftVal === null && rightVal !== null)
             return lessThan;
-        else if (rightVal == null && leftVal != null)
+        else if (rightVal === null && leftVal !== null)
             return greaterThan;
 
-        return leftVal == rightVal ? 0 : (leftVal < rightVal ? lessThan : greaterThan);
+        return leftVal === rightVal ? 0 : (leftVal < rightVal ? lessThan : greaterThan);
     };
 
     // Properties
     self.Model = ko.observable(null);
     self.SearchQuery = ko.observable($.cookie('BrowseImplementationGuide_SearchQuery') || '');
     self.SortField = ko.observable($.cookie('BrowseImplementationGuide_SortField') || 'Name');
-    self.SortAscending = ko.observable($.cookie('BrowseImplementationGuide_SortAscending') == "false" ? false : true);
+    self.SortAscending = ko.observable($.cookie('BrowseImplementationGuide_SortAscending') === "false" ? false : true);
+    self.FilterType = ko.observable($.cookie('BrowseImplementationGuide_FilterType') || '');
+    self.FilterStatus = ko.observable($.cookie('BrowseImplementationGuide_FilterStatus') || '');
+    self.FilterOrganization = ko.observable($.cookie('BrowseImplementationGuide_FilterOrganization') || '');
     self.UnauthorizedImplementationGuides = ko.observableArray([]);
     self.AccessRequestMessage = ko.observable();
     self.RequestEditAccess = ko.observable(false);
     self.RequestAccessMessage = ko.observable('');
+    self.Types = ko.observableArray(self.FilterType() ? [self.FilterType()] : []);
+    self.Statuses = ko.observableArray(self.FilterStatus() ? [self.FilterStatus()] : []);
+    self.Organizations = ko.observableArray(self.FilterOrganization() ? [self.FilterOrganization()] : []);
 
     var storeCookies = function() {
         $.cookie('BrowseImplementationGuide_SortField', self.SortField());
         $.cookie('BrowseImplementationGuide_SortAscending', self.SortAscending());
         $.cookie('BrowseImplementationGuide_SearchQuery', self.SearchQuery());
+        $.cookie('BrowseImplementationGuide_FilterType', self.FilterType());
+        $.cookie('BrowseImplementationGuide_FilterStatus', self.FilterStatus());
+        $.cookie('BrowseImplementationGuide_FilterOrganization', self.FilterOrganization());
     };
 
     self.SearchQuery.subscribe(storeCookies);
     self.SortField.subscribe(storeCookies);
     self.SortAscending.subscribe(storeCookies);
+    self.FilterType.subscribe(storeCookies);
+    self.FilterStatus.subscribe(storeCookies);
+    self.FilterOrganization.subscribe(storeCookies);
+
+    self.GetSortIcon = function (sortField) {
+        return {
+            'glyphicon-text-height': self.SortField() !== sortField,
+            'glyphicon-sort-by-alphabet': self.SortField() === sortField && self.SortAscending(),
+            'glyphicon-sort-by-alphabet-alt': self.SortField() === sortField && !self.SortAscending()
+        };
+    };
 
     self.RequestEditAccessString = ko.computed({
         read: function () {
@@ -75,7 +99,7 @@
     });
 
     self.ShowEdit = ko.computed(function () {
-        return !listMode || listMode == 'Default';
+        return !listMode || listMode === 'Default';
     });
 
     self.Title = ko.computed(function () {
@@ -112,6 +136,31 @@
             success: function (model) {
                 ko.mapping.fromJS({ Model: model }, {}, self);
                 self.Model().Items.sort(sortItems);
+
+                var types = _.chain(self.Model().Items())
+                    .map(function (item) {
+                        return item.Type();
+                    })
+                    .uniq()
+                    .value();
+                self.Types(types);
+
+                var statuses = _.chain(self.Model().Items())
+                    .map(function (item) {
+                        return item.Status();
+                    })
+                    .uniq()
+                    .value();
+                self.Statuses(statuses);
+
+                var organizations = _.chain(self.Model().Items())
+                    .map(function (item) {
+                        return item.Organization();
+                    })
+                    .uniq()
+                    .value();
+                self.Organizations(organizations);
+
                 fireTrifoliaEvent('implementationGuidesLoaded');
             }
         });
@@ -136,7 +185,7 @@
     self.RequestAccess = function (implementationGuideId) {
         self.AccessRequestMessage('');
         var implementationGuide = ko.utils.arrayFirst(self.UnauthorizedImplementationGuides(), function (item) {
-            return item.Id() == implementationGuideId;
+            return item.Id() === implementationGuideId;
         });
 
         var url = '/api/ImplementationGuide/' + implementationGuideId + '/RequestAuthorization';
@@ -158,7 +207,7 @@
     };
 
     self.ToggleSort = function (sortField) {
-        if (self.SortField() == sortField) {
+        if (self.SortField() === sortField) {
             self.SortAscending(!self.SortAscending());
         } else {
             self.SortField(sortField);
@@ -169,11 +218,16 @@
     };
 
     self.GetItems = ko.computed(function () {
+        var filterType = self.FilterType();
+        var filterStatus = self.FilterStatus();
+        var filterOrganization = self.FilterOrganization();
+        var searchQuery = self.SearchQuery();
+
         if (!self.Model()) {
             return [];
         }
 
-        if (self.SearchQuery().length == 0) {
+        if (!searchQuery && !filterType) {
             return self.Model().Items();
         }
 
@@ -184,22 +238,34 @@
             var item = self.Model().Items()[i];
             var itemDate = formatDateObj(item.PublishDate());
 
-            var matched = false;
+            var matched = !searchQuery;
 
-            if (regexp.test(item.Title())) {
+            if (searchQuery && regexp.test(item.Title())) {
                 matched = true;
             }
 
-            if (regexp.test(itemDate)) {
+            if (searchQuery && regexp.test(itemDate)) {
                 matched = true;
             }
 
-            if (regexp.test(item.Status())) {
+            if (searchQuery && regexp.test(item.Status())) {
                 matched = true;
             }
 
-            if (regexp.test(item.Organization())) {
+            if (searchQuery && regexp.test(item.Organization())) {
                 matched = true;
+            }
+
+            if (self.FilterType() && item.Type() !== filterType) {
+                matched = false;
+            }
+
+            if (self.FilterStatus() && item.Status() != filterStatus) {
+                matched = false;
+            }
+
+            if (self.FilterOrganization() && item.Organization() != filterOrganization) {
+                matched = false;
             }
 
             if (matched) {
