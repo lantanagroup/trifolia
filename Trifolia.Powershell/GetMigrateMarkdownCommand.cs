d25d83bf8efd29e702a5c1a1e74570ab179fe559
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2018-04-05 12:10:44 -0700

    Adding migration for converting HTML in ImplementationGuideSection.Content to Markdown. May need to be expanded to convert tables into markdown as well.

--- Trifolia.Powershell/GetMigrateMarkdownCommand.cs
+++ Trifolia.Powershell/GetMigrateMarkdownCommand.cs
@@ -14,7 +14,9 @@ namespace Trifolia.Powershell
 {
     public class MigrateMarkdownLog
     {
-        public int TemplateId { get; set; }
+        public int ImplementationGuideId { get; set; }
+        public int? SectionId { get; set; }
+        public int? TemplateId { get; set; }
         public int? ConstraintId { get; set; }
         public string Property { get; set; }
         public string Original { get; set; }
@@ -22,11 +24,11 @@ namespace Trifolia.Powershell
     }
 
     [Cmdlet(VerbsCommon.Get, "MigrateMarkdown")]
-    public class MigrateMarkdownCommand : BaseCommand
+    public class GetMigrateMarkdownCommand : BaseCommand
     {
         private List<MigrateMarkdownLog> logs;
 
-        private string ConvertWikiToMarkdown(string wiki, int templateId, string property, int? constraintId = null)
+        private string ConvertWikiToMarkdown(string wiki, int implementationGuideId, int templateId, string property, int? constraintId = null)
         {
             var converter = new Html2Markdown.Converter();
             string markdown = wiki;
@@ -66,6 +68,31 @@ namespace Trifolia.Powershell
             return markdown;
         }
 
+        public string ConvertHtmlToMarkdown(string html, int implementationGuideId, int sectionId, string property)
+        {
+            string cleanHtml = html
+                .Replace("<div>", "")
+                .Replace("</div>", "")
+                .Replace("<br>", "")
+                .Replace("<br />", "")
+                .Replace("<br/>", "");
+
+            var converter = new Html2Markdown.Converter();
+            string markdown = converter.Convert(cleanHtml);
+
+            if (html != markdown)
+                this.logs.Add(new MigrateMarkdownLog()
+                {
+                    ImplementationGuideId = implementationGuideId,
+                    SectionId = sectionId,
+                    Property = property,
+                    Original = html,
+                    New = markdown
+                });
+
+            return !string.IsNullOrEmpty(markdown) ? markdown : html;
+        }
+
         /// <summary>
         /// Converts the following fields to MarkDown format:
         /// - Template.Description
@@ -75,25 +102,34 @@ namespace Trifolia.Powershell
         {
             this.logs = new List<MigrateMarkdownLog>();
 
+            foreach (var implementationGuide in this.tdb.ImplementationGuides)
+            {
+                foreach (var section in implementationGuide.Sections.Where(y => !string.IsNullOrEmpty(y.Content)))
+                {
+                    section.Content = ConvertHtmlToMarkdown(section.Content, implementationGuide.Id, section.Id, "Content");
+                    this.WriteVerbose("Updated implementation guide " + implementationGuide.Id + "'s section " + section.Id + " to be markdown");
+                }
+            }
+
             foreach (var template in this.tdb.Templates)
             {
                 if (!string.IsNullOrEmpty(template.Description))
                 {
-                    template.Description = this.ConvertWikiToMarkdown(template.Description, template.Id, "Description");
-                    this.WriteVerbose("Updated template " + template.Id.ToString() + "'s description to be markdown");
+                    template.Description = this.ConvertWikiToMarkdown(template.Description, template.OwningImplementationGuideId, template.Id, "Description");
+                    this.WriteVerbose("Updated template " + template.Id + "'s description to be markdown");
                 }
 
                 if (!string.IsNullOrEmpty(template.Notes))
                 {
-                    template.Notes = this.ConvertWikiToMarkdown(template.Notes, template.Id, "Notes");
-                    this.WriteVerbose("Updated template " + template.Id.ToString() + "'s notes to be markdown");
+                    template.Notes = this.ConvertWikiToMarkdown(template.Notes, template.OwningImplementationGuideId, template.Id, "Notes");
+                    this.WriteVerbose("Updated template " + template.Id + "'s notes to be markdown");
                 }
 
                 foreach (var constraint in template.ChildConstraints.Where(y => !string.IsNullOrEmpty(y.PrimitiveText)))
                 {
                     if (!string.IsNullOrEmpty(constraint.PrimitiveText))
                     {
-                        constraint.PrimitiveText = this.ConvertWikiToMarkdown(constraint.PrimitiveText, template.Id, "PrimitiveText", constraint.Id);
+                        constraint.PrimitiveText = this.ConvertWikiToMarkdown(constraint.PrimitiveText, template.OwningImplementationGuideId, template.Id, "PrimitiveText", constraint.Id);
                         this.WriteVerbose(string.Format("Update template {0}'s constraint {1}'s PrimitiveText to be markdown",
                             template.Id,
                             constraint.Id));
