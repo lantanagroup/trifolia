0b5c10392d6252dfb97b87c79f3edca16e260e9d
ARVIND-PC\Arvind <arvind.jagannathan@lantanagroup.com>
2018-04-11 14:41:51 -0400

    TRIF-1308: Refactored IGTypePluginFactory to select a plugin based on their attributes

--- Trifolia.Plugins/IGTypePluginFactory.cs
+++ Trifolia.Plugins/IGTypePluginFactory.cs
@@ -14,24 +14,34 @@ namespace Trifolia.Plugins
     {
         public static IIGTypePlugin GetPlugin(ImplementationGuideType igType)
         {
-            /*
-            IGTypeSection config = IGTypeSection.GetSection();
-            IGTypePluginElement configElement = config.Plugins[igType.Name];
+            //Collect all methods in script
+            Assembly assembly = Assembly.GetExecutingAssembly();
 
-            if (configElement == null)
-                throw new Exception("Plugin not configured for this type of schema!");
+            //Find all IGPlugins
+            var IGPlugins = assembly.GetTypes()
+                .Where(t => t.IsClass && t.GetCustomAttributes(typeof(ImplementationGuideTypePluginAttribute)).Any())
+                .ToArray();
 
-            Type type = Type.GetType(configElement.PluginAssembly);
+            
+            foreach(var IGPlugin in IGPlugins)
+            {
+                //Get attributes of each plugin being examined
+                var attributes = IGPlugin.GetCustomAttributes();
+                
+                //Check to see if the plugin is of the correct type by examining its attributes
+                var pluginAtt = attributes.SingleOrDefault(a => a.GetType() == typeof(ImplementationGuideTypePluginAttribute) && ((ImplementationGuideTypePluginAttribute)a).IGType == igType.Name);
 
-            if (type == null)
-                throw new Exception("Plugin not loaded for IG Type: " + configElement.PluginAssembly);
+                //If pluginAtt isn't null, means the examined IGPlugin is of the right type
+                if(pluginAtt != null)
+                {
+                    //Create an instance of the plugin and return it
+                    var plugin = (IIGTypePlugin)Activator.CreateInstance(IGPlugin);
+                    return plugin;
+                }
+            }
 
-            var pluginInterface = (IIGTypePlugin)Activator.CreateInstance(type);
-            return pluginInterface;
-            */
-
-            // TODO: Fill in logic to find an IIGTypePlugin that matches the igType.Name based on ImplementationGuideTypeAttribute.IGType
-            return null;
+            //IG type plugin we're searching for doesn't exist (should never be the case but seems like a reasonable thing to have a check for)
+            throw new NotSupportedException();
         }
     }
 
